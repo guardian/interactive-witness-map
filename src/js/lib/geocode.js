ed649f01f439e24bf890327d952d0fe1a82d2507
Will Franklin <will.franklin@guardian.co.uk>
2015-09-30 17:06:39 +0100

    Add proximity

--- src/js/lib/geocode.js
+++ src/js/lib/geocode.js
@@ -4,7 +4,7 @@ const key = 'pk.eyJ1IjoiZ3VhcmRpYW4iLCJhIjoiNHk1bnF4OCJ9.25tK75EuDdgq5GxQKyD6Fg'
 
 export default function geocode(query, fn) {
     reqwest({
-        'url': 'https://api.mapbox.com/v4/geocode/mapbox.places/' + encodeURIComponent(query) + '.json?access_token=' + key,
+        'url': 'https://api.mapbox.com/v4/geocode/mapbox.places/' + encodeURIComponent(query) + '.json?proximity=-0.092703,51.514456&access_token=' + key,
         'type': 'json',
         'crossOrigin': true,
         'error': err => fn(err),
